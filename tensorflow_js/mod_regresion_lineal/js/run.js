2dfe16beec4dc657d0d50e89bee65451ec0190eb
cristian-rincon <cristian.o.rincon.b@gmail.com>
2020-07-30 15:58:30 -0500

    modelo terminado

--- tensorflow_js/mod_regresion_lineal/js/run.js
+++
@@ -1,7 +0,0 @@
-import visualizarDatos from "./visualizarDatos.js";
-import { getData } from "./main";
-export async function run() {
-    const data = await getData();
-    //console.log(data);
-    visualizarDatos(data);
-}
