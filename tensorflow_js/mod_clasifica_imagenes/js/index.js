a26a61e20c16c30edb58a4051f2cd06d54b7e5d0
cristian-rincon <cristian.o.rincon.b@gmail.com>
2020-07-30 21:51:33 -0500

    modelo de clasificacion de imagenes

---
+++ tensorflow_js/mod_clasifica_imagenes/js/index.js
@@ -0,0 +1,46 @@
+let net;
+
+const imgElement = document.getElementById("img");
+const descElement = document.getElementById("descripcion_imagen");
+const webcamElement = document.getElementById("webcam");
+const descElementVideo = document.getElementById("descripcion_video");
+
+function utils(number) {
+    return Math.floor(number)
+}
+
+async function app() {
+  net = await mobilenet.load();
+  //const result = await net.classify(imgElement);
+  //console.log(result);
+  displayImagePrediction();
+
+  const webcam = await tf.data.webcam(webcamElement);
+  while (true) {
+    const img = await webcam.capture();
+    const result = await net.classify(img);
+    descElementVideo.innerHTML =
+      ` Predicción: ${result[0].className}<br> Probabilidad: ${utils(result[0].probability * 100)} %`;
+  }
+}
+
+imgElement.onload = async () => {
+  displayImagePrediction();
+};
+
+async function displayImagePrediction() {
+  try {
+    const result = await net.classify(imgElement);
+    descElement.innerHTML =
+      ` Predicción: ${result[0].className}<br> Probabilidad: ${utils(result[0].probability * 100)} %`;
+  } catch (error) {
+    console.error(error);
+  }
+}
+var count = 0;
+async function cambiarImagen() {
+  count = count + 1;
+  imgElement.src = `https://picsum.photos/200/300?random=${count}`;
+}
+
+app();
