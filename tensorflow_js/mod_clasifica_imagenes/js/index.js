3b371b4d003d406ee9d8f884bc7a2601553d61b6
cristian-rincon <cristian.o.rincon.b@gmail.com>
2020-08-05 19:46:36 -0500

    tfjs

--- tensorflow_js/mod_clasifica_imagenes/js/index.js
+++ tensorflow_js/mod_clasifica_imagenes/js/index.js
@@ -1,48 +1,63 @@
-let net;
+let model;
 
-const imgElement = document.getElementById("img");
-const descElement = document.getElementById("descripcion_imagen");
 const webcamElement = document.getElementById("webcam");
-const descElementVideo = document.getElementById("descripcion_video");
+const classifier = knnClassifier.create();
 
-function utils(number) {
-  return Math.floor(number);
-}
-
+const imgEl = document.getElementById("img");
+const descEl = document.getElementById("descripcion_imagen");
+var count = 0;
+var net;
+var webcam;
 async function app() {
+  console.log("Cargando modelo de identificacion de imagenes");
   net = await mobilenet.load();
-  //const result = await net.classify(imgElement);
-  //console.log(result);
-  displayImagePrediction();
+  console.log("Carga terminada");
+  //clasificamos la imagen de carga
+  const result = await net.classify(imgEl);
+  console.log(result);
+  descEl.innerHTML = JSON.stringify(result);
 
-  const webcam = await tf.data.webcam(webcamElement);
+  //obtenemos datos del webcam
+  webcam = await tf.data.webcam(webcamElement);
+  //y los vamos procesando
   while (true) {
     const img = await webcam.capture();
+
     const result = await net.classify(img);
-    descElementVideo.innerHTML = ` Predicción: ${
-      result[0].className
-    }<br> Probabilidad: ${utils(result[0].probability * 100)} %`;
+
+    const activation = net.infer(img, "conv_preds");
+    var result2;
+    try {
+      result2 = await classifier.predictClass(activation);
+    } catch (error) {
+      result2 = {};
+    }
+
+    document.getElementById("descripcion_video").innerText = `
+      prediction: ${result[0].className}\n
+      probability: ${result[0].probability}
+    `;
+
+    // Dispose the tensor to release the memory.
+    img.dispose();
+
+    // Give some breathing room by waiting for the next animation frame to
+    // fire.
+    await tf.nextFrame();
   }
 }
 
-imgElement.onload = async () => {
-  displayImagePrediction();
+img.onload = async function () {
+  try {
+    result = await net.classify(img);
+    descEl.innerHTML = JSON.stringify(result);
+  } catch (error) {}
 };
 
-async function displayImagePrediction() {
-  try {
-    const result = await net.classify(imgElement);
-    descElement.innerHTML = ` Predicción: ${
-      result[0].className
-    }<br> Probabilidad: ${utils(result[0].probability * 100)} %`;
-  } catch (error) {
-    console.error(error);
-  }
-}
-var count = 0;
 async function cambiarImagen() {
   count = count + 1;
-  imgElement.src = `https://picsum.photos/200/300?random=${count}`;
+  imgEl.src = "https://picsum.photos/200/300?random=" + count;
+  descEl.innerHTML = "";
 }
 
 app();
