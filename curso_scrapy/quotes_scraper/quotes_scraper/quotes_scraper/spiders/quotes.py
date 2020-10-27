e26a7bee3fc203f7effc59efe9c083faf4aa44db
cristian-rincon <cristian.o.rincon.b@gmail.com>
2020-10-26 23:22:32 -0500

    se generan configuraciones adicionales

--- curso_scrapy/quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes.py
+++ curso_scrapy/quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -1,16 +1,19 @@
 import scrapy
-
+import datetime
 # Utilites
 from .common import config
 import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
+TIME = datetime.datetime.now()
 TITLE = str(config()['scraper']['quotes_scraper']['titulo'])
 QUOTES = str(config()['scraper']['quotes_scraper']['citas'])
 TOP_TAGS = str(config()['scraper']['quotes_scraper']['top_tags'])
 NEXT_PAGE_BUTTON = str(
     config()['scraper']['quotes_scraper']['next_page_button'])
+MEMUSAGE_NOTIFY_MAIL = str(
+    config()['scraper']['quotes_scraper']['memusage_notify_mail'])
+    
 
 
 class QuotesSpider(scrapy.Spider):
@@ -23,8 +26,14 @@ class QuotesSpider(scrapy.Spider):
     ]
     # Settings to save in file
     custom_settings = {
-        'FEED_URI': f'results/{name}.json',
-        'FEED_FORMAT': 'json'
+        'FEED_URI': f'results/{name}_{TIME.strftime("%d-%b-%Y_%H-%M-%S")}.json',
+        'FEED_FORMAT': 'json',
+        'CONCURRENT_REQUESTS': 24, # Number of concurrent requests
+        'MEMUSAGE_LIMITS_MB': 2048,
+        'MEMUSAGE_NOTIFY_MAIL': [MEMUSAGE_NOTIFY_MAIL],
+        'ROBOTSTXT_OBEY': True,
+        'USER_AGENT': 'CristianRincon',
+        'FEED_EXPORT_ENCODING': 'utf-8'
     }
 
     def parse_only_quotes(self, response, **kwargs):
@@ -49,6 +58,7 @@ class QuotesSpider(scrapy.Spider):
 
         title = response.xpath(TITLE).get()
         quotes = response.xpath(QUOTES).getall()
+        # TODO - Traer los autores de las quotes
         top_tags = response.xpath(TOP_TAGS).getall()
 
         top = getattr(self, 'top', None)
