cf6ca6c0f27732cc4db65a8f97251a6dfa41002d
cristian-rincon <cristian.o.rincon.b@gmail.com>
2021-02-13 12:37:19 -0500

    Page Object Model (POM)

---
+++ intro_selenium/metodologias_de_trabajo/pom/test_google.py
@@ -0,0 +1,28 @@
+import unittest
+
+from pyunitreport import HTMLTestRunner
+from selenium import webdriver
+
+from google_page import GooglePage
+
+
+class GoogleTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.driver = webdriver.Chrome(executable_path='../../chromedriver')
+
+    def test_search(self):
+        google = GooglePage(self.driver)
+        google.open()
+        google.search('Platzi')
+        self.assertEqual('Platzi', google.keyword)
+
+    @classmethod
+    def tearDown(cls) -> None:
+        cls.driver.close()
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2, testRunner=HTMLTestRunner(
+        output='.',
+        report_name=__file__))
